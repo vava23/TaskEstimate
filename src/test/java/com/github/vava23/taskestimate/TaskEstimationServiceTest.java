43211a98011543c2f782732c549452bdf83be9cc
vava23 <svv15@mail.ru>
2020-04-01 17:21:20 +0300

    Improved tests

--- src/test/java/com/github/vava23/taskestimate/TaskEstimationServiceTest.java
+++ src/test/java/com/github/vava23/taskestimate/TaskEstimationServiceTest.java
@@ -1,18 +1,38 @@
 package com.github.vava23.taskestimate;
 
+import com.github.vava23.taskestimate.domain.Estimate;
+import com.github.vava23.taskestimate.domain.Task;
 import com.github.vava23.taskestimate.domain.TaskEstimationService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-//
-// Task completion estimation based on PERT technique
-//
-public class TaskEstimationServiceTest {
+import java.util.ArrayList;
+import java.util.List;
 
-    //private final TaskEstimationPERT estimation = new TaskEstimationPERT();
+/**
+ * Task completion estimation based on PERT technique
+ */
+public class TaskEstimationServiceTest {
+    /** Compare delta */
+    static final double DELTA = 0.01; // UI precision is 0.1
+    /** Reference values */
+    static final double REF_INPUT_ML = 5;
+    static final double REF_INPUT_BC = 2;
+    static final double REF_INPUT_WC = 14;
+    static final Estimate REF_ESTIMATE = new Estimate(6, 2);
+    static final List<Task> REF_TASKS = new ArrayList<Task>();
+    static final Estimate REF_ESTIMATE_TOTAL = new Estimate(11, 1.892969449);
+
+    /** Initialization */
+    static {
+        // Initialize reference estimate list
+        REF_TASKS.add(new Task(1, "ONE", 3, 2, 10));
+        REF_TASKS.add(new Task(2, "TWO", 1, 0.5, 1.5));
+        REF_TASKS.add(new Task(3, "THREE", 5, 4, 12));
+    }
 
     @Test
-    void calcTaskEstimateTest() {
+    static void calcTaskEstimateTest() {
         // Incorrect input
         boolean thrown = true;
         try {
@@ -43,11 +63,13 @@ public class TaskEstimationServiceTest {
         Assert.assertTrue(thrown);
 
         // Correct input
-        TaskEstimationService.calcTaskEstimate(5, 1, 10);
+        Estimate estimate = TaskEstimationService.calcTaskEstimate(REF_INPUT_ML, REF_INPUT_BC, REF_INPUT_WC);
+        Assert.assertEquals(estimate.getDays(), REF_ESTIMATE.getDays(), DELTA);
+        Assert.assertEquals(estimate.getStDeviation(), REF_ESTIMATE.getStDeviation(), DELTA);
     }
 
     @Test
-    void combineEstimatesTest() {
+    static void combineEstimatesTest() {
         // Null list on input
         boolean thrown = true;
         try {
@@ -56,6 +78,16 @@ public class TaskEstimationServiceTest {
         }
         catch (IllegalArgumentException e) { }
         Assert.assertTrue(thrown);
+
+        // Correct input
+        Estimate estimate = TaskEstimationService.combineEstimates(REF_TASKS);
+        Assert.assertEquals(estimate.getDays(), REF_ESTIMATE_TOTAL.getDays(), DELTA);
+        Assert.assertEquals(estimate.getStDeviation(), REF_ESTIMATE_TOTAL.getStDeviation(), DELTA);
     }
 
+    /** Run all tests */
+    public static void main(String[] args) {
+        calcTaskEstimateTest();
+        combineEstimatesTest();
+    }
 }
